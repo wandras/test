45a6ffef83d36255f9723050cb6960d39364c1f0
landzup <landzup@gmail.com>
2019-10-04 09:27:31 +0200

    Update on-off.js

--- on-off.js
+++ on-off.js
@@ -5,42 +5,42 @@
 */
 
 Window.prototype.on = HTMLDocument.prototype.on = Element.prototype.on = function(eventName, callback, options) {
-    if (!('eventListenersList' in this)) {
-        // create a cache of event listeners attached:
-        this.eventListenersList = [];
-    }
-    
-    if ('addEventListener' in this) {
-        // W3C compliant method
-        if (typeof(options) === 'boolean') {
-            var optionsValue = options;
-        } else if (!!options) {
-            var optionsValue = {
-                useCapture: 'capture' in options ? options.capture || false,
-                once: 'once' in options ? options.once || false,
-                passive: 'passive' in options ? options.passive || false
-            };
-        }
-        
-        // cache the listeners added to the target:
-        this.eventListenersList.push([eventName, callback, optionsValue]);
-        return this.addEventListener(eventName, callback, optionsValue);
-    } else {
-        // environment using attachEvent and detachEvent methods
-        // cache the listeners added to the target:
-        this.eventListenersList.push([eventName, callback]);
-        return this.attachEvent('on' + eventName, callback);
-    }
+	if (!('eventListenersList' in this)) {
+		// create a cache of event listeners attached:
+		this.eventListenersList = [];
+	}
+	
+	if ('addEventListener' in this) {
+		// W3C compliant method
+		if (typeof(options) === 'boolean') {
+			var optionsValue = options;
+		} else if (!!options) {
+			var optionsValue = {
+				useCapture: 'capture' in options ? options.capture || false,
+				once: 'once' in options ? options.once || false,
+				passive: 'passive' in options ? options.passive || false
+			};
+		}
+		
+		// cache the listeners added to the target:
+		this.eventListenersList.push([eventName, callback, optionsValue]);
+		return this.addEventListener(eventName, callback, optionsValue);
+	} else {
+		// environment using attachEvent and detachEvent methods
+		// cache the listeners added to the target:
+		this.eventListenersList.push([eventName, callback]);
+		return this.attachEvent('on' + eventName, callback);
+	}
 };
 
 Window.prototype.off = HTMLDocument.prototype.off = Element.prototype.off = function(eventName, callback, useCapture) {
-    if (!('eventListenersList' in this)) {
-        // create a cache of event listeners attached, if not existing:
-        this.eventListenersList = [];
-    }
+	if (!('eventListenersList' in this)) {
+		// create a cache of event listeners attached, if not existing:
+		this.eventListenersList = [];
+	}
 	
 	if ('removeEventListener' in this) {
-        // W3C compliant method
+		// W3C compliant method
 		if (arguments.length > 0) {
 			// eventName given, remove only the specified listener
 			this.removeEventListener(eventName, callback, useCapture);
@@ -59,7 +59,7 @@ Window.prototype.off = HTMLDocument.prototype.off = Element.prototype.off = func
 			});
 		}
 	} else {
-        // environment using attachEvent and detachEvent methods
+		// environment using attachEvent and detachEvent methods
 		if (arguments.length > 0) {
 			// eventName given, remove only the specified listener
 			this.detachEvent('on' + eventName, callback);
